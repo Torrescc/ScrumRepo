83d50b6ea0abae23f592a6f5b70544f19c2e0790
Chris Torres <torrescc@augsburg.edu>
2024-09-19 15:17:08 -0500

    Uploading Calculator Java files

---
+++ Calculator.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Calculator{
+    public static void main(String[] args){
+        Scanner scrn = new Scanner(System.in);
+        int x = scrn.nextInt();
+        int y = scrn.nextInt();
+
+        System.out.println("x: " + x);
+        System.out.println("y: " + y);
+    }
+}
